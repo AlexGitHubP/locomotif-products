f4c6c9df3eb556d04de998315ef806ccd33673c7
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2023-05-07 16:31:16 +0300

    Add files via upload

--- src/ProductsServiceProvider.php
+++ src/ProductsServiceProvider.php
@@ -24,8 +24,12 @@ class ProductsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/Routes/routes.php');
-        $this->loadViewsFrom(__DIR__.'/views', 'products');
-        $this->loadViewsFrom(__DIR__.'/views/attributes', 'attributes');
+        $this->loadViewsFrom(__DIR__.'/views',                              'products');
+        $this->loadViewsFrom(__DIR__.'/views/categories',                   'categories');
+        $this->loadViewsFrom(__DIR__.'/views/subcategories',                'subcategories');
+        $this->loadViewsFrom(__DIR__.'/views/attributes',                   'attributes');
+        $this->loadViewsFrom(__DIR__.'/views/areas',                        'areas');
+        $this->loadViewsFrom(__DIR__.'/views/attributes/attributes_values', 'attributes_values');
         
         $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
 
